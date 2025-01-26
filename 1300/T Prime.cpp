d4eaa7c0106205ccf2d143706800622731905032
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-01-26 14:12:36 +0600

    add

---
+++ 1300/T Prime.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<long long> prime(1000001, 0); // সাইজ বাড়ানো হয়েছে 1000001 পর্যন্ত
+
+    prime[1] = 1;
+    for (long long i = 2; i * i <= 1000000; i++) {
+        if (prime[i] == 0) {
+            for (long long j = i * i; j <= 1000000; j += i) {
+                prime[j] = 1;
+            }
+        }
+    }
+    
+    int n;
+    cin >> n;
+    while (n--) {
+        long long x;
+        cin >> x;
+        long long root = sqrt(x);
+        if (root * root == x && prime[root] == 0) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
