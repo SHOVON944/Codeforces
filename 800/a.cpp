5f60a50442ee036bd7e64dcbf7e1ea4b2a7cd29f
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-02-04 18:54:04 +0600

    add

--- 800/a.cpp
+++ 800/a.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
-#include <string>
+#include<iomanip>
 using namespace std;
 
 int main()
 {
-    int t;
-    cin>>t;
-    while (t--){
-        string s;
-        cin>>s;
-        int str = 0;
-        for(char c : s){
-            if(c == '1'){
-                str++;
-            }
+    long long n;
+    cin>>n;
+    if(n>1){
+        for(int i=1; i<=n; i++){
+            if(i%2==0) cout<<i<<endl;
         }
-        cout<<str<<endl;
+    } else{
+        cout<<"-1";
     }
+
     return 0;
 }
