96631cc87df7a4848a649f046fc028dfda776207
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-02-13 01:15:18 +0600

    add

--- 900/practice.cpp
+++ 900/practice.cpp
@@ -1,35 +1,47 @@
-#include <iostream>
-#include <string>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    string remix;
-    cin >> remix;  // Read input string
 
-    // Replace all "WUB" with spaces
-    while (remix.find("WUB") != string::npos) {
-        remix.replace(remix.find("WUB"), 3, " ");
+    // store prefixSum
+void prefixSum(int arr[], int prefix[], int n){
+    prefix[0] = arr[0];         // store 1st index( 0th index)
+    for(int i=1; i<n; i++){
+        prefix[i] = prefix[i-1] + arr[i];
+    }
+}
+
+    //  sum the range value
+int rangeSum(int prefix[], int l, int h){
+    if(l==0) return prefix[h];
+    else return prefix[h] - prefix[l-1];
+}
+
+
+
+int main()
+{
+    int n;
+    cout<<"Enter array size : ";
+    cin>>n;
+
+    int arr[n], prefix[n];
+
+    cout<<"Enter "<<n<<"Elements : ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
     }
 
-    // Remove extra spaces
-    string original_song;
-    bool space = false;
-    for (char c : remix) {
-        if (c != ' ') {
-            original_song += c;
-            space = true;
-        } else if (space) {
-            original_song += ' ';
-            space = false;
-        }
-    }
+    // store prefix sum through function call
+    prefixSum(arr, prefix, n);
 
-    // Trim trailing space if present
-    if (!original_song.empty() && original_song.back() == ' ') {
-        original_song.pop_back();
-    }
+    // input range(l -> lowest, h -> highest)
+    int l, h;
+    cout<<"Enter range(l:lowest, h:highest): ";
+    cin>>l>>h;
 
-    cout << original_song << endl;
+    int sum = rangeSum(prefix, l, h);
+
+    cout<<"Sum from index "<<l<<" to "<<h<<" is: "<<sum;
+
     return 0;
 }
