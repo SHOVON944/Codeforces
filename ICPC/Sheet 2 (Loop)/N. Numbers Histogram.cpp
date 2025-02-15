4e7b1ce7c0c126dbe010ad5fd4bc6db9723450cb
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-02-15 22:23:35 +0600

    add

---
+++ ICPC/Sheet 2 (Loop)/N. Numbers Histogram.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    string s;
+    cin>>s;
+    int n;
+    cin>>n;
+    while(n--){
+        int x;
+        cin>>x;
+        for(int i=1; i<=x; i++){
+            cout<<s;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
