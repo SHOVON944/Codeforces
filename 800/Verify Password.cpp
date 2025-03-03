68eaa68de100d7d78561e2b8e9583fc676c45c8e
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-03-03 13:50:29 +0600

    add

---
+++ 800/Verify Password.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int size;
+        cin>>size;
+        string pass;
+        cin>>pass;
+        bool pass_verify = true;
+        for(int i=1; i<size; i++){
+            if(pass[i]<pass[i-1]){
+                pass_verify = false;
+                break;
+            }
+        }
+        if(pass_verify) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+ 
+    return 0;
+}
--- 800/a.cpp
+++ 800/a.cpp
@@ -1,30 +1,11 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-    
-    int k = (n * n + 1) / 2;  // Position of the middle element in the sorted list
-    
-    int low = 1, high = n * n, mid, count;
-    
-    while (low < high) {
-        mid = (low + high) / 2;
-        count = 0;
-        
-        // Count numbers <= mid in the multiplication table
-        for (int i = 1; i <= n; i++) {
-            count += min(mid / i, n);
-        }
-        
-        if (count < k)
-            low = mid + 1;
-        else
-            high = mid;
-    }
-    
-    cout << low << endl;
-    return 0;
+int main()
+{
+    vector<int> arpita;
+    arpita.push_back('O');
+for(int arpi: arpita){
+    cout<<arpi<<endl;
+}
+  return 0;
 }
