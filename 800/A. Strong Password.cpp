a0ead7a65211cf086dd1f9e34f7a8a6edabfacdf
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-03-04 14:06:26 +0600

    add

---
+++ 800/A. Strong Password.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        string pass;
+        cin>>pass;
+        for(int i=1; i<pass.size(); i++){
+            if(pass[i]==pass[i-1]){
+                
+            }
+        }
+    }
+
+    return 0;
+}
