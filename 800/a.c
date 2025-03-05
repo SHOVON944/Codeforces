834aba1a4252b0fc0da13cb7ef81988b6a3a943b
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-03-05 20:25:18 +0600

    add

--- 800/a.c
+++ 800/a.c
@@ -2,5 +2,35 @@
 
 int main()
 {
+    int eye, mouth, body;
+    scanf("%d",&eye);
+    scanf("%d",&mouth);
+    scanf("%d",&body);
+    int count = 0;
+    while(1){
+        if(eye>0 && mouth>0 && body>0){
+        count++;
+        eye--;
+        mouth--;
+        body--;
+    }
+    else if(eye>1 && mouth>0 && body>0){
+        count++;
+        eye--;
+        mouth--;
+        body--;
+    }
+        if(eye>1  && body>0){
+        count++;
+        eye--;
+        mouth--;
+        body--;
+    }
+}
+
+    printf("%d",count);
+    
+
+
     return 0;
 }
--- 800/a.cpp
+++ 800/a.cpp
@@ -3,19 +3,24 @@ using namespace std;
 
 int main()
 {
-  int n;
-  cin>>n;
-  vector<int> h(n);
-  vector<int> ans(n);
-  for(int i=0; i<n; i++){
-    cin>>h[i];
-    }
-    ans[0] = 0;
-    ans[1]= abs(h[1]-h[0]);
-    for(int i=2; i<n; i++){
-      ans[i] = min(ans[i-1] + abs(h[i]-h[i-1]), ans[i-2] + abs(h[i]-h[i-2]));
-    }
-    cout<<ans[n-1]<<endl;
+  int eye, mouth, body;
+  cin>>eye>>mouth>>body;
+  int store = 0;
+  int case1 = min(eye, min(mouth, body));
+  store = store + case1;
+  
+  if(body< eye  &&  body<mouth) cout<<body<<endl;
+
+  eye = eye - store*2;
+  mouth = mouth - store;
+  body = body - store;
+
+  if(eye>1  &&  mouth>0){
+    store = store + min(eye/2, mouth);
+    eye = eye - (min(eye/2, mouth))/2;
+  }
+
+  cout<<store<<endl;
 
   return 0;
 }
