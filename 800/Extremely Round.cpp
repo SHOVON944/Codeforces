2bb5099d1aa80522769f0da1ea6350a41925c92b
MD. SAKIBUL ISLAM SHOVON <mdsakibulislamshovon944@gmail.com>
2025-03-06 05:40:13 +0600

    Update Extremely Round.cpp

--- 800/Extremely Round.cpp
+++ 800/Extremely Round.cpp
@@ -11,11 +11,11 @@ int main()
         int size = s.size();
         int convertTo_int;
         if(size==1){
-            convertTo_int = s[0] - '0';
+            convertTo_int = s[0] - '0';                    // 1 to n 
             cout << convertTo_int << endl;
         } else{
             convertTo_int = s[0] - '0';
-            int ans = convertTo_int + (9*(size-1));
+            int ans = convertTo_int + (9*(size-1));                        // 1-9=9,	10-99=9,	100-999=9 and so on
             cout << ans << endl;
         }
     }
