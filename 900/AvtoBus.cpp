8b7cfbbca5b80823a4b32456d536253820798163
MD. SAKIBUL ISLAM SHOVON <mdsakibulislamshovon944@gmail.com>
2025-03-10 13:24:51 +0600

    Update AvtoBus.cpp

--- 900/AvtoBus.cpp
+++ 900/AvtoBus.cpp
@@ -9,11 +9,12 @@ int main()
         long long n;
         cin >> n;
         if(n%2!=0) cout << "-1" << endl;
+        else if(n==2) cout << "-1" << endl;
+        else if(n==4) cout<<"1"<<" 1" <<endl;
+        else if(n==6) cout << "1"<<" 1" << endl;
         else{
-            if(n==2) cout << "-1" << endl;
-            else if(n%4==0  &&  n%6==0) cout<<n/6<<" "<<n/4<<endl;
-            else if(n%4==0) cout << "1"<<" 1" << endl;
-            else if(n%6==0) cout << "1"<<" 1" << endl;
+            if(n%6==0) cout << n/6<<" "<<n/4 << endl;
+            else cout<<n/6+1<<" "<<n/4<<endl;
         }
     }
     
