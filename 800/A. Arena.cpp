26219482382b1842dd1a6252e7cc34739082fa9a
MD. SAKIBUL ISLAM SHOVON <mdsakibulislamshovon944@gmail.com>
2025-03-12 11:49:33 +0600

    Update A. Arena.cpp

--- 800/A. Arena.cpp
+++ 800/A. Arena.cpp
@@ -13,8 +13,8 @@ int main()
             cin >>war[i];
         }
         sort(war, war+n);
-        if(war[0]==war[1]) cout << n-2 << endl;
-        else cout << n-1 << endl;
+        int counter  = count(war, war+n, war[0]);
+        cout << n-counter << endl;
     }
     
     return 0;
