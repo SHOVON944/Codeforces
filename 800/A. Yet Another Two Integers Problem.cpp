9a7bb8052667166734346be54efe944a4814a34e
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-03-12 22:31:04 +0600

    add

---
+++ 800/A. Yet Another Two Integers Problem.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int a, b;
+        cin>>a>>b;
+        int diff = abs(a-b);
+        int step = (diff/10) + (diff%10);
+        cout<<step<<endl;
+    }
+
+    return 0;
+}
