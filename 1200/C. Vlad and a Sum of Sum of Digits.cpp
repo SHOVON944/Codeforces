82b8fa717922b4e00809fa051a96368439526210
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-03-25 10:55:05 +0600

    add

--- 1200/C. Vlad and a Sum of Sum of Digits.cpp
+++ 1200/C. Vlad and a Sum of Sum of Digits.cpp
@@ -3,19 +3,23 @@ using namespace std;
 
 int main()
 {
-    int t;  cin>>t;
-    while(t--){
-        long long x;   cin>>x;
-        long long sum = 0;
-        for(int i=1; i<=x; i++){
+    int result[200007];
+        result[0] = 0;
+        for(int i=1; i<200007; i++){
             int num = i;
+            long long sum = 0;
             while(num>0){
                 int remaining = num%10;
                 sum += remaining;
                 num /=10;
             }
+            result[i] = result[i-1] + sum;
         }
-        cout<<sum<<endl;
+        
+    int t;  cin>>t;
+    while(t--){
+        long long x;   cin>>x;
+        cout<<result[x]<<endl;
     }
 
     return 0;
