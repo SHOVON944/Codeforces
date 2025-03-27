ccefc3e2fc55abe5ba35b1c246828ff11b63b849
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-03-27 23:33:51 +0600

    add

---
+++ 800/A. Magical Sticks.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;  cin>>t;
+    while(t--){
+        int x;  cin>>x;
+        int ans = x+1;
+        cout<<ans/2<<endl;
+    }
+}
