5fb0b233cfb58ebb53120cdcfbd229fac412fddf
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-05-03 09:58:39 +0600

    add

--- 800/A. Equation.cpp
+++ 800/A. Equation.cpp
@@ -6,8 +6,8 @@ int main()
     long long n;   cin>>n;
 
     if(n==1) cout<<"9 8"<<endl;
-    if(n%2==0) cout<<n+4<<" 4"<<endl;
-    if(n%2!=0) cout<<n+9<<" 9"<<endl;
+    else if(n%2==0) cout<<n+4<<" 4"<<endl;
+    else cout<<n+9<<" 9"<<endl;
 
     return 0;
 }
