8e47dbcbfb2a747669a529d3319f2f2f880dbef8
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-06-15 22:18:28 +0600

    add

--- B_Make_Majority.cpp
+++ B_Make_Majority.cpp
@@ -7,8 +7,14 @@ int main()
     while(t--){
         int sizeN;   cin>>sizeN;
         string num;   cin>>num;
-        if(num[0] == '1'   &&   num[sizeN-1] =='1') cout<<"Yes"<<endl;
-        else cout<<"No"<<endl;
+        int count_1 = 0;
+        int count_0 = 0;
+        for(char c : num){
+            if(c == '1') count_1++;
+            else count_0++;
+        }
+        if(count_1>count_0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
     }
 
     return 0;
