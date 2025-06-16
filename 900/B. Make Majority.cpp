8bc74fede3cc940f5283464d68cc995565d57181
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-06-16 16:17:09 +0600

    add

--- 900/B. Make Majority.cpp
+++ 900/B. Make Majority.cpp
@@ -1,41 +1,50 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-void solve() {
-    int n;
-    string s;
-    cin >> n >> s;
 
 
-    int ones = 0;
-    int zero = 0;
-    for (char c : s) {
-        if (c == '1') ones++;
-        else zero++;
-    }
-    if (ones > zero) {
-        cout << "YES\n";
-        return;
-    }
+// solve hoini ekhono...
 
-    else {
-        cout << "NO\n";
-        return ;
-    }
 
-        if (n == 1) {
-        cout << (s[0] == '1' ? "YES\n" : "NO\n");
-        return;
-    }
-}
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
-    return 0;
-}
+
+
+
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// void solve() {
+//     int n;
+//     string s;
+//     cin >> n >> s;
+
+
+//     int ones = 0;
+//     int zero = 0;
+//     for (char c : s) {
+//         if (c == '1') ones++;
+//         else zero++;
+//     }
+//     if (ones > zero) {
+//         cout << "YES\n";
+//         return;
+//     }
+
+//     else {
+//         cout << "NO\n";
+//         return ;
+//     }
+
+//         if (n == 1) {
+//         cout << (s[0] == '1' ? "YES\n" : "NO\n");
+//         return;
+//     }
+// }
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         solve();
+//     }
+//     return 0;
+// }
