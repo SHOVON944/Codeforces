857a5c8162e86352ff49821ea9ad98c57b6b9293
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-06-19 18:56:05 +0600

    add

--- 1000/A. cAPS lOCK.cpp
+++ 1000/A. cAPS lOCK.cpp
@@ -1,15 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    string capslocks;
-    cin>>capslocks;
-    for(char &ch : capslocks){
-        ch = tolower(ch);
+    string cAPS;
+    cin>>cAPS;
+    string new_str = string(cAPS.begin()+1, cAPS.end());
+    if(all_of(cAPS.begin(), cAPS.end(), [](char c){return isupper(c);})){
+        for(char &ch : cAPS){
+            ch = tolower(ch);
+        }
+        cout << cAPS << endl;
+        return 0;
+    } else if(islower(cAPS[0])){
+        if(all_of(cAPS.begin()+1, cAPS.end(), [](char c){return isupper(c);})){
+            for(char &ch :new_str){
+            ch = tolower(ch);
+        	}
+            cout << (char)toupper(cAPS[0])<< new_str << endl;
+            return 0;
+        }
     }
-    capslocks[0] = toupper(capslocks[0]);
-    for(char &ch : capslocks){
-        cout<<ch;
-    }
+    cout << cAPS << endl;
+    
+    return 0;
 }
