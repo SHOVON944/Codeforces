dedc3ddb36252287d80f9886ef09c2374a55d94b
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-06-29 17:37:35 +0600

    add

--- 800/A. Quintomania.cpp
+++ 800/A. Quintomania.cpp
@@ -13,6 +13,7 @@ int main()
         for(int i=0; i<n; i++){
             cin>>note[i];
         }
+
         for(int i=1; i<n; i++){
             if((abs(note[i]-note[i-1]) == 5)  ||  (abs(note[i]-note[i-1]) == 7)){
                 check = true;
