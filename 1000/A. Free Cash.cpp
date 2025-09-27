4812564751e502a5eeb27a9053fe09165abc8da2
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-09-27 08:30:32 +0600

    add

--- 1000/A. Free Cash.cpp
+++ 1000/A. Free Cash.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin>>n;
+    map <pair<int, int>, int> count;
+
+    int h, m;
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        cin>>h>>m;
+        count[{h, m}]++;
+        ans = max(ans, count[{h, m}]);
+    }
+    cout<<ans;
 
     return 0;
 }
