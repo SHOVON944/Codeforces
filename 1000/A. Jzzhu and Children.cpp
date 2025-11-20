c3cfba1984d8876aa945fed3f2c69babff9ce158
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-11-20 17:50:11 +0600

    addadd

--- 1000/A. Jzzhu and Children.cpp
+++ 1000/A. Jzzhu and Children.cpp
@@ -11,17 +11,15 @@ int main()
 
     int maxRound = 0;
     int lastChild = 1;
-
     int round = 0;
     for(int i=0 ; i<n; i++){
-        round = ceil(children[i]/a);
+        round = ((children[i] + a - 1 ) / a); // ceil(children[i]/a)
         if(round>=maxRound){
             maxRound = round;
             lastChild = i +1;
         }
         round = 0;
     }
-
     cout<<lastChild;
 
     return 0;
